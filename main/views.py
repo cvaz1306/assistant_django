8f3ebe7f2e80a5b44d4761e5eba868984247e590
BuildTools <unconfigured@null.spigotmc.org>
2023-09-02 16:06:07 -0500

    Attempting to fix request.POST.get() in views.py line 14

--- main/views.py
+++ main/views.py
@@ -10,13 +10,13 @@ def main(request):
 @csrf_exempt
 def mess(request):
     print(f"Request method: {request.method}")
-    if request.method == "POST":
-        message_text = request.POST.get('message', None)
-        print(f"Creating db entry: {message_text}")
-        # Create a new message object and save it to the database
-        message = models.message.objects.create(message=message_text)
-        message.message=message_text
-        message.save()
+    #if request.method == "GET":
+    message_text = request.POST.get("message")
+    print(f"Creating db entry: {message_text}")
+    # Create a new message object and save it to the database
+    message = models.message.objects.create(message=message_text)
+    message.message=message_text
+    message.save()
 
     # Retrieve all messages from the database and prepare them for JSON response
     all_messages = [message.message for message in models.message.objects.all()]
