3dc15047a7631b91df0d9cc4ebcd0921a48be178
BuildTools <unconfigured@null.spigotmc.org>
2023-09-02 14:20:48 -0500

    Removed '/' on attatched file in message schema

--- main/models.py
+++ main/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class message(models.Model):
     message=models.TextField(null=True)
-    attatched_file=models.FileField(upload_to='/resources', null=True)
+    attatched_file=models.FileField(upload_to='resources', null=True)
     filename=models.TextField(null=False, max_length=50)
