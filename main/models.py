d239fb2090a9c9219357a7c4b0a5a1e9e9915e4d
BuildTools <unconfigured@null.spigotmc.org>
2023-09-02 14:14:58 -0500

    Created message db schema

--- main/models.py
+++ main/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class message(models.Model):
     message=models.TextField(null=True)
     attatched_file=models.FileField(upload_to='/resources', null=True)
+    filename=models.TextField(null=False, max_length=50)
--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from . import texpprocessing
+from . import models
 # Create your views here.
 
 def main(request):
     return render(template_name='main/main.html', request=request)
 
 def mess(request):
-    
+    return JsonResponse({message.message for message in models.message})
