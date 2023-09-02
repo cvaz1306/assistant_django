3b4d7ebbc8c8b25266598877ef22c46d9c444e1c
BuildTools <unconfigured@null.spigotmc.org>
2023-09-02 16:57:48 -0500

    Fixed message processing issue

--- main/views.py
+++ main/views.py
@@ -10,16 +10,16 @@ def main(request):
     return render(template_name='main/main.html', request=request)
 @csrf_exempt
 def mess(request):
+    postData=json.loads(request.body)
     print(f"Request method: {request.method}")
-    #if request.method == "GET":
-    message_text = json.dumps(request.POST, indent=4)#).get('message','No message Available')
-    print(f"Creating db entry: {message_text}")
-    # Create a new message object and save it to the database
-    message = models.message.objects.create(message=message_text)
-    message.message=message_text
-    message.save()
+    print(str(request.body))
+    if request.method == "POST":
+        # Create a new message object and save it to the database
+        message = models.message(message=postData.get('message',None))
+        message.save()
+        print(f"Created Message: {message.message}")
 
     # Retrieve all messages from the database and prepare them for JSON response
-    all_messages = [message.message for message in models.message.objects.all()]
+    all_messages = [{"message":message.message, 'filename':message.filename} for message in models.message.objects.all()]
 
     return JsonResponse(all_messages, safe=False)
