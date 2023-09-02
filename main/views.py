aca0c06352e55889a8a285c2669808ad97c9efbf
BuildTools <unconfigured@null.spigotmc.org>
2023-09-02 15:23:11 -0500

    Made mess view crsf exempt

--- main/views.py
+++ main/views.py
@@ -2,11 +2,12 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from . import texpprocessing
 from . import models
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def main(request):
     return render(template_name='main/main.html', request=request)
-
+@csrf_exempt
 def mess(request):
     if(request.method=='POST' or request.method=="POST"):
         if(request.POST.get('message',None) is not None):
