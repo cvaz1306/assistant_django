1de75e4c4b71da05f4a55b9f7da68227e9681b29
BuildTools <unconfigured@null.spigotmc.org>
2023-09-02 14:56:11 -0500

    Added handling for new messages

--- main/views.py
+++ main/views.py
@@ -8,4 +8,8 @@ def main(request):
     return render(template_name='main/main.html', request=request)
 
 def mess(request):
-    return JsonResponse({message.message for message in models.message})
+    if(request.method=='POST' or request.method=="POST"):
+        if(request.POST.get('message',None) is not None):
+            message=request.POST.get('message',None)
+            message=models.message(message=request.POST.get('message',None))
+    return JsonResponse({message.message for message in models.message.objects.all()})
