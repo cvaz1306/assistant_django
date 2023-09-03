1e532d67681511217816895a51c0ebb2ab35d3e2
BuildTools <unconfigured@null.spigotmc.org>
2023-09-03 15:48:44 -0500

    Addes main.css

--- assistant_django/settings.py
+++ assistant_django/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/Users/kx838/Docs/assistant/assistant_django/static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
