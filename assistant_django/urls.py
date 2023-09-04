b91552cc625405e871c362d33ed8b378179691ce
BuildTools <unconfigured@null.spigotmc.org>
2023-09-04 15:07:41 -0500

    File hosting functionality added

--- assistant_django/urls.py
+++ assistant_django/urls.py
@@ -21,5 +21,5 @@ from django.urls import include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("main/",include("main.urls")),
-    path('filemanager/', include("filemanager.urls"))
+    path('filemanager/', include("filemanager.urls")),
 ]
