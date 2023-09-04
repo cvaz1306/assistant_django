322033ca4a8ea28d8a22599c5e9f25898023575b
BuildTools <unconfigured@null.spigotmc.org>
2023-09-03 21:02:18 -0500

    Added static directory, worked out branches

--- assistant_django/settings.py
+++ assistant_django/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'main',
-    'filemanager',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/Users/kx838/Docs/assistant/assistant_django/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
