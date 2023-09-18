26316fe40352ca6f5046921640658949a53735a0
BuildTools <unconfigured@null.spigotmc.org>
2023-09-17 18:33:29 -0700

    Fixed file issues

--- static/js/filemanager.js
+++ static/js/filemanager.js
@@ -59,3 +59,53 @@ document.addEventListener('submit', (e) => {
 
 });
 // Trigger file input click when the plus button is clicked
+const uploadButton = document.querySelector('.upload-button');
+const fileInput = document.getElementById('file-input');
+fileInput.addEventListener('click', (event) => {
+    if (!e) {
+        event.preventDefault()
+    }
+    console.log("Clicked")
+
+})
+uploadButton.addEventListener('click', () => {
+    e = true;
+    console.log("Clicked button")
+    fileInput.click();
+    e = false;
+    updateFiles();
+});
+
+// Prevent Enter key from triggering file input click
+uploadButton.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+    }
+});
+
+// Handle file selection (you can add further logic here)
+fileInput.addEventListener('change', () => {
+    // You can access selected file(s) via fileInput.files
+    const selectedFile = fileInput.files[0];
+    if (selectedFile) {
+        // Handle the selected file, e.g., upload it to the server
+        console.log(`Selected file: ${selectedFile.name}`);
+        document.getElementById("sub").click();
+    }
+    updateFiles();
+});
+document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log("Sending");
+    // Store reference to form to make later code easier to read
+    const form = e.target;
+
+    // Post data using the Fetch API
+    fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+    });
+
+    // Prevent the default form submit
+    updateFiles();
+});
